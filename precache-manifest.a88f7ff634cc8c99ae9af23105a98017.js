02f4f139a5e6002a395ab2a827ffe963a2eb1d52
zsevic <zsevic95@gmail.com>
2019-03-26 12:55:30 +0100

    Updates

---
+++ precache-manifest.a88f7ff634cc8c99ae9af23105a98017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaa68e2fa84b773691a4",
+    "url": "/tracklister/static/js/runtime~main.acbad179.js"
+  },
+  {
+    "revision": "762cac5bae67413f7903",
+    "url": "/tracklister/static/js/main.e09a9a05.chunk.js"
+  },
+  {
+    "revision": "1686cd28f6bc37d38267",
+    "url": "/tracklister/static/js/2.8337ce21.chunk.js"
+  },
+  {
+    "revision": "1686cd28f6bc37d38267",
+    "url": "/tracklister/static/css/2.00038b43.chunk.css"
+  },
+  {
+    "revision": "9aef6108327447e8e8137b9264e2e2d2",
+    "url": "/tracklister/index.html"
+  }
+];
