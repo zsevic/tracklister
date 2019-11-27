4b77b551aa184a0b44c32e8143ec425d085a0a93
zsevic <zsevic95@gmail.com>
2019-11-27 01:53:21 +0100

    Updates

---
+++ precache-manifest.5118e1c59311d15e930d38710c37f43f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bda0e7735e78cde112c75b5eea3b5e5",
+    "url": "/tracklister/index.html"
+  },
+  {
+    "revision": "7583ab0847795ae1e6f2",
+    "url": "/tracklister/static/css/2.5c5f4c00.chunk.css"
+  },
+  {
+    "revision": "7583ab0847795ae1e6f2",
+    "url": "/tracklister/static/js/2.051bde66.chunk.js"
+  },
+  {
+    "revision": "0129c4753bde38a0385c",
+    "url": "/tracklister/static/js/main.2c899742.chunk.js"
+  },
+  {
+    "revision": "d9358617be1cf3c9883c",
+    "url": "/tracklister/static/js/runtime-main.305d2a4e.js"
+  }
+]);
